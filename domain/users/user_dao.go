25dcc4b6ad664b9350c969fb3eaea45083274116
Mai Tung <goingdownfr@gmail.com>
2020-06-16 22:12:37 +0700

    insert row to db

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,9 +2,16 @@ package users
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/maitungmn/bookstore_users-api-go/datasources/mysql/users_db"
 	"github.com/maitungmn/bookstore_users-api-go/utils/date_utils"
 	"github.com/maitungmn/bookstore_users-api-go/utils/errors"
+)
+
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
 )
 
 var (
@@ -12,6 +19,9 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
 	result := usersDB[user.ID]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %v not found", user.ID))
@@ -26,16 +36,28 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.ID]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %v already registed", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %v already exists", user.ID))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
 	user.DateCreated = date_utils.GetNowString()
 
-	usersDB[user.ID] = user
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(fmt.Sprintf("email %v already exist", user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %v \n", err.Error()))
+	}
+
+	userID, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %v \n", err.Error()))
+	}
+
+	user.ID = userID
+
 	return nil
 }
