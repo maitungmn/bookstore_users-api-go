a333de81b26c26ebe351503d57a29b08462dee36
Mai Tung <goingdownfr@gmail.com>
2020-06-19 12:17:28 +0700

    create access token

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -107,3 +107,18 @@ func Search(c *gin.Context) {
 
 	c.JSON(http.StatusOK, users.Marshal(c.GetHeader("x-Public") == "true"))
 }
+
+func Login(c *gin.Context) {
+	var request users.LoginRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+	}
+
+	user, err := services.UsersService.LoginUser(request)
+	if err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+	c.JSON(http.StatusOK, user.Marshal(c.GetHeader("x-Public") == "true"))
+}
